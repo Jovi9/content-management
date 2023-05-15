255d49e1804f4819a0e0f771db42ea1277bcaeec
Joshua Villafuerte <joshuar.villafuerte9@gmail.com>
2023-05-15 17:09:26 +0800

    Modified public page layout

--- resources/views/public/custom-page.blade.php
+++ resources/views/public/custom-page.blade.php
@@ -3,94 +3,92 @@
 @section('title', $menuName)
 
 @section('content')
-    <section id="banner" style="background-image: url({{ asset('storage/logo/site_banner.png') }})">
-        <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true"
-                    aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            </div>
-            <div class="carousel-inner">
-                <div class="carousel-item active carousel-item-page">
-                    <img src="{{ asset('storage/images/1.jpg') }}" class="d-block w-100 carousel-img" alt="Image">
-
-                    <div class="container">
-                        <div class="carousel-caption text-start">
-                            <h1>CoEd Spearheads BUROGKOS: a team building activity, enchances gender sensitive powerhouse
-                            </h1>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item carousel-item-page">
-                    <img src="{{ asset('storage/images/2.jpg') }}" class="d-block w-100 carousel-img" alt="Image">
-
-                    <div class="container">
-                        <div class="carousel-caption text-start">
-                            <h1>CoEd Spearheads BUROGKOS: a team building activity, enchances gender sensitive powerhouse
-                            </h1>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item carousel-item-page">
-                    <img src="{{ asset('storage/images/3.jpg') }}" class="d-block w-100 carousel-img" alt="Image">
-
-                    <div class="container">
-                        <div class="carousel-caption text-start">
-                            <h1>CoEd Spearheads BUROGKOS: a team building activity, enchances gender sensitive powerhouse
-                            </h1>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
+    <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true"
+                aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
         </div>
-        <div class="b-divider"></div>
-        <section class="py-5"></section>
+        <div class="carousel-inner">
+            <div class="carousel-item active carousel-item-page">
+                <img src="{{ asset('storage/images/1.jpg') }}" class="d-block w-100 carousel-img" alt="Image">
 
-        @if (!$contents->isEmpty())
-            @if ($contents)
-                @foreach ($contents as $content)
-                    <div class="container py-3" id="{{ strtolower(str_replace(' ', '-', $content->title)) }}">
-                        <div class="row justify-content-center">
-                            <div class="card">
-                                <div class="card-body">
-                                    <div class="bg-white">
-                                        <div class="container px-4 py-4">
-                                            <h2 class="pb-2 border-bottom border-4">{{ $content->title }}</h2>
-                                            <div class="row mt-4">
-                                                {!! $content->content !!}
-                                            </div>
+                <div class="container">
+                    <div class="carousel-caption text-start">
+                        <h1>CoEd Spearheads BUROGKOS: a team building activity, enchances gender sensitive powerhouse
+                        </h1>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item carousel-item-page">
+                <img src="{{ asset('storage/images/2.jpg') }}" class="d-block w-100 carousel-img" alt="Image">
+
+                <div class="container">
+                    <div class="carousel-caption text-start">
+                        <h1>CoEd Spearheads BUROGKOS: a team building activity, enchances gender sensitive powerhouse
+                        </h1>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item carousel-item-page">
+                <img src="{{ asset('storage/images/3.jpg') }}" class="d-block w-100 carousel-img" alt="Image">
+
+                <div class="container">
+                    <div class="carousel-caption text-start">
+                        <h1>CoEd Spearheads BUROGKOS: a team building activity, enchances gender sensitive powerhouse
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    <div class="b-divider"></div>
+    <section class="py-5"></section>
+
+    @if (!$contents->isEmpty())
+        @if ($contents)
+            @foreach ($contents as $content)
+                <div class="container py-3" id="{{ strtolower(str_replace(' ', '-', $content->title)) }}">
+                    <div class="row justify-content-center">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="bg-white">
+                                    <div class="container px-4 py-4">
+                                        <h2 class="pb-2 border-bottom border-4">{{ $content->title }}</h2>
+                                        <div class="row mt-4">
+                                            {!! $content->content !!}
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                @endforeach
-            @endif
-        @else
-            <div class="d-flex align-items-center justify-content-center p-5" style="height:50vh;">
-                <div class="text-center text-white">
-                    {{-- <h1 class="display-1 fw-bold">404</h1> --}}
-                    <p class="fs-3"> <span class="text-danger">Opps!</span> Empty Page.</p>
-                    <p class="lead">
-                        The page you’re looking for currently have no content.
-                    </p>
-                    <a href="{{ route('public-home') }}" class="btn btn-primary">Go Home</a>
                 </div>
-            </div>
+            @endforeach
         @endif
+    @else
+        <div class="d-flex align-items-center justify-content-center p-5" style="height:50vh;">
+            <div class="text-center">
+                {{-- <h1 class="display-1 fw-bold">404</h1> --}}
+                <p class="fs-3"> <span class="text-danger">Opps!</span> Empty Page.</p>
+                <p class="lead">
+                    The page you’re looking for currently have no content.
+                </p>
+                <a href="{{ route('public-home') }}" class="btn btn-primary">Go Home</a>
+            </div>
+        </div>
+    @endif
 
-        <section class="py-5"></section>
-    </section>
+    <section class="py-5"></section>
 @endsection
 
 @section('scripts')
--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -58,9 +58,11 @@
         </button>
     </div>
 
-    <section class="py-5" id="banner" style="background-image: url({{ asset('storage/logo/site_banner.png') }})">
-        <div class="container-fluid mb-5 mt-5">
-            <h2 class="pb-2 border-bottom border-4 mb-5 mt-5 container text-white"><strong>News and Updates</strong></h2>
+    <div class="b-gray-divider"></div>
+
+    <section class="py-5 bg-white">
+        <div class="container mb-5 mt-5">
+            <h2 class="pb-2 border-bottom border-4 mb-5 mt-5 container"><strong>News and Updates</strong></h2>
             <div class="slide-container p-4 swiper">
                 <div class="slide-content px-3 overflow-hidden">
                     <div class="slide-card-contents swiper-wrapper">
@@ -211,121 +213,73 @@
         </div>
     </section>
 
-    <div class="b-divider"></div>
+    <div class="b-gray-divider"></div>
 
-    <section class="featured">
+    <section class="bg-white">
         <div class="container py-5">
             <div class="container mt-5 mb-5">
-                <h2 class="pb-2 border-bottom border-4 text-white mt-5 mb-5"><strong>Featured</strong></h2>
+                <h2 class="pb-2 border-bottom border-4 mt-5 mb-5"><strong>Featured</strong></h2>
             </div>
             <div class="row justify-content-center mb-5">
                 <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-4 mb-5">
-                    <div class="col">
-                        <div class="card" style="width: 18rem;">
-                            <img src="{{ asset('storage/images/3.jpg') }}" class="card-img-top" alt="Image">
-                            <div class="card-body">
-                                <h5 class="card-title"><strong>Card title</strong></h5>
-                                <p class="card-text cut-text">Words in the woods are found anywhere in the world. Words
-                                    in the woods are found around the world.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
+                    <div class="col d-flex align-items-start">
+                        <div
+                            class="icon-square text-bg-light d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
+                            <i class="fas fa-microchip p-3"></i>
+                        </div>
+                        <div>
+                            <h3 class="fs-2">Featured title</h3>
+                            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
+                                sentence and
+                                probably just keep going until we run out of words.</p>
+                            <a href="#" class="btn btn-primary">
+                                Primary button
+                            </a>
                         </div>
                     </div>
-                    <div class="col">
-                        <div class="card" style="width: 18rem;">
-                            <img src="{{ asset('storage/images/3.jpg') }}" class="card-img-top" alt="Image">
-                            <div class="card-body">
-                                <h5 class="card-title"><strong>Card title</strong></h5>
-                                <p class="card-text cut-text">Words in the woods are found anywhere in the world. Words
-                                    in the woods are found around the world.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
+                    <div class="col d-flex align-items-start">
+                        <div
+                            class="icon-square text-bg-light d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
+                            <i class="fas fa-microchip p-3"></i>
+                        </div>
+                        <div>
+                            <h3 class="fs-2">Featured title</h3>
+                            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
+                                sentence and
+                                probably just keep going until we run out of words.</p>
+                            <a href="#" class="btn btn-primary">
+                                Primary button
+                            </a>
                         </div>
                     </div>
-                    <div class="col">
-                        <div class="card" style="width: 18rem;">
-                            <img src="{{ asset('storage/images/3.jpg') }}" class="card-img-top" alt="Image">
-                            <div class="card-body">
-                                <h5 class="card-title"><strong>Card title</strong></h5>
-                                <p class="card-text cut-text">Words in the woods are found anywhere in the world. Words
-                                    in the woods are found around the world.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
+                    <div class="col d-flex align-items-start">
+                        <div
+                            class="icon-square text-bg-light d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
+                            <i class="fas fa-microchip p-3"></i>
+                        </div>
+                        <div>
+                            <h3 class="fs-2">Featured title</h3>
+                            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
+                                sentence and
+                                probably just keep going until we run out of words.</p>
+                            <a href="#" class="btn btn-primary">
+                                Primary button
+                            </a>
                         </div>
                     </div>
-                    <div class="col">
-                        <div class="card" style="width: 18rem;">
-                            <img src="{{ asset('storage/images/3.jpg') }}" class="card-img-top" alt="Image">
-                            <div class="card-body">
-                                <h5 class="card-title"><strong>Card title</strong></h5>
-                                <p class="card-text cut-text">Words in the woods are found anywhere in the world. Words
-                                    in the woods are found around the world.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
+                    <div class="col d-flex align-items-start">
+                        <div
+                            class="icon-square text-bg-light d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
+                            <i class="fas fa-microchip p-3"></i>
                         </div>
-                    </div>
-                    <div class="col">
-                        <div class="card" style="width: 18rem;">
-                            <img src="{{ asset('storage/images/3.jpg') }}" class="card-img-top" alt="Image">
-                            <div class="card-body">
-                                <h5 class="card-title"><strong>Card title</strong></h5>
-                                <p class="card-text cut-text">Words in the woods are found anywhere in the world. Words
-                                    in the woods are found around the world.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card" style="width: 18rem;">
-                            <img src="{{ asset('storage/images/3.jpg') }}" class="card-img-top" alt="Image">
-                            <div class="card-body">
-                                <h5 class="card-title"><strong>Card title</strong></h5>
-                                <p class="card-text cut-text">Words in the woods are found anywhere in the world. Words
-                                    in the woods are found around the world.Words in the woods are found anywhere in the
-                                    world. Words
-                                    in the woods are found around the world.Words in the woods are found anywhere in the
-                                    world. Words
-                                    in the woods are found around the world.Words in the woods are found anywhere in the
-                                    world. Words
-                                    in the woods are found around the world.Words in the woods are found anywhere in the
-                                    world. Words
-                                    in the woods are found around the world.Words in the woods are found anywhere in the
-                                    world. Words
-                                    in the woods are found around the world.Words in the woods are found anywhere in the
-                                    world. Words
-                                    in the woods are found around the world.Words in the woods are found anywhere in the
-                                    world. Words
-                                    in the woods are found around the world.Words in the woods are found anywhere in the
-                                    world. Words
-                                    in the woods are found around the world.Words in the woods are found anywhere in the
-                                    world. Words
-                                    in the woods are found around the world.Words in the woods are found anywhere in the
-                                    world. Words
-                                    in the woods are found around the world.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card" style="width: 18rem;">
-                            <img src="{{ asset('storage/images/3.jpg') }}" class="card-img-top" alt="Image">
-                            <div class="card-body">
-                                <h5 class="card-title"><strong>Card title</strong></h5>
-                                <p class="card-text cut-text">Words in the woods are found anywhere in the world. Words
-                                    in the woods are found around the world.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card" style="width: 18rem;">
-                            <img src="{{ asset('storage/images/3.jpg') }}" class="card-img-top" alt="Image">
-                            <div class="card-body">
-                                <h5 class="card-title"><strong>Card title</strong></h5>
-                                <p class="card-text cut-text">Words in the woods are found anywhere in the world. Words
-                                    in the woods are found around the world.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
+                        <div>
+                            <h3 class="fs-2">Featured title</h3>
+                            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
+                                sentence and
+                                probably just keep going until we run out of words.</p>
+                            <a href="#" class="btn btn-primary">
+                                Primary button
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -333,9 +287,11 @@
         </div>
     </section>
 
-    <section class="py-5" id="banner" style="background-image: url({{ asset('storage/logo/site_banner.png') }})">
+    <div class="b-gray-divider"></div>
+
+    <section class="py-5 bg-white">
         <div class="container mt-5 mb-5">
-            <h2 class="pb-2 border-bottom border-4 text-white mt-5 mb-5"><strong>Gallery</strong></h2>
+            <h2 class="pb-2 border-bottom border-4 mt-5 mb-5"><strong>Gallery</strong></h2>
         </div>
         <div class="container">
             <div class="row justify-content-center mb-5">
@@ -395,12 +351,12 @@
         </div>
     </section>
 
-    <div class="b-divider"></div>
+    <div class="b-gray-divider"></div>
 
-    <section class="featured">
+    <section class="bg-white">
         <div class="container py-5">
             <div class="container mt-5 mb-5">
-                <h2 class="pb-2 border-bottom border-4 text-white mt-5 mb-5"><strong>Partners</strong></h2>
+                <h2 class="pb-2 border-bottom border-4 mt-5 mb-5"><strong>Partners</strong></h2>
             </div>
             <div class="row justify-content-md-center mb-5">
                 <div class="row row-cols-2 row-cols-md-4 row-cols-lg-6 g-4 mb-5">
@@ -454,15 +410,14 @@
         </div>
     </section>
 
-    <section class="py-5" id="banner" style="background-image: url({{ asset('storage/logo/site_banner.png') }})">
+    <div class="b-gray-divider"></div>
 
-    </section>
 @endsection
 
 @section('scripts')
     <script>
         var swiper = new Swiper(".slide-content", {
-            slidesPerView: 6,
+            slidesPerView: 5,
             spaceBetween: 30,
             loop: false,
             centerSlide: true,
@@ -490,12 +445,6 @@
                 },
                 1230: {
                     slidesPerView: 4,
-                },
-                1600: {
-                    slidesPerView: 5,
-                },
-                2000: {
-                    slidesPerView: 6,
                 },
             }
         });
